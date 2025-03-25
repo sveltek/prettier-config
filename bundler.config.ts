e47ec20100aee0a088336cf155cb7a3669c594fd
Ivo Dolenc <ivodolenc@gmail.com>
2025-03-25 18:03:51 +0100

    chore: update main bundler config

---
+++ bundler.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from '@hypernym/bundler'
+
+export default defineConfig({
+  entries: [
+    { input: './src/index.js' },
+    { dts: './src/types.ts', output: './dist/index.d.mts' },
+  ],
+})
