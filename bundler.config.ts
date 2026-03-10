51aa81cf2eddb6b86a256f0aac85e9e17272e4f8
Ivo Dolenc <ivodolenc@gmail.com>
2026-03-10 15:56:51 +0100

    chore: add externals

--- bundler.config.ts
+++ bundler.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from '@hypernym/bundler'
+import { defineConfig, externals } from '@hypernym/bundler'
+import { dependencies, devDependencies } from './package.json'
 
 export default defineConfig({
+  externals: [
+    ...externals,
+    ...Object.keys(dependencies),
+    ...Object.keys(devDependencies),
+  ],
   entries: [
-    { input: './src/index.js' },
+    { input: './src/index.ts' },
     { dts: './src/types.ts', output: './dist/index.d.ts' },
   ],
 })
